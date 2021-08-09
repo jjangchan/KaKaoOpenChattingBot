6a425e09b437315c695be2efd8b3515044cf4cf3
jjangchan <rudcks6806@gamil.com>
2021-08-09 11:41:12 +0900

    CSV 파일 타임프레임 함수 추가

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ if __name__ == '__main__':
     path = "Binance_BTCUSDT_minute.csv"
     BinanceGetHistoricalData.ReadCSVFile(path)
 
-    #unix = int("1567970000000")/1000
-    #print(datetime.utcfromtimestamp(unix).strftime('%Y.%m.%d'))
-    #print(datetime.utcfromtimestamp(unix).strftime('%H:%M'))
 
 
 
