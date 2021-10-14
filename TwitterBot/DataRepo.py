ecc8d4a024c9ccb23a7bc58dfec57d2e2678a471
jjangchan <rudcks6806@gamil.com>
2021-10-14 10:01:25 +0900

    commit

--- TwitterBot/DataRepo.py
+++ TwitterBot/DataRepo.py
@@ -17,17 +17,18 @@ class Data(object) :
     str_current_url = ""
     list_account = []
     list_telegram_account = []
+    sleep_second = 0
 
     def __init__(self, str_file_name):
         self.str_file_name = str_file_name
         self.__LoadJson()
         self.ConnectTwitter()
         self.ConnectTelegram()
-        current_time = int(time.time())-(60*60*9)
+        current_time = int(time.time())-(60*60*9) #GMT+0
         self.dict_time = {account: current_time for account in Data.list_account}
-        self.GetTwitterData("@jjangchan22")
         self.working = True
 
+    # 트위터 연결
     def ConnectTwitter(self):
         Data.twitter_api = twitter.Api()
         try:
@@ -38,6 +39,7 @@ class Data(object) :
         except Exception as e:
             print("Connect(twitter) error = ", e)
 
+    # 텔레그램 연결
     def ConnectTelegram(self):
         try:
             Data.telegram_bot = telegram.Bot(token=Data.telegram_token)
@@ -49,7 +51,7 @@ class Data(object) :
         except Exception as e:
             print("Connect(telegram) error = ", e)
 
-
+    # 트위터 특정 계정 타임라인 데이터 가져오기
     def GetTwitterData(self, account):
         list_ref = []
         try:
@@ -59,7 +61,7 @@ class Data(object) :
                                                               exclude_replies=True)
             if len(twitter_status) > 0:
                 last_time = self.StrToUnixTime(twitter_status[0].created_at)
-                if last_time > self.dict_time[account]:
+                if last_time > self.dict_time[account]: # 새로 트윗한글 가져오기
                     for status in twitter_status:
                         current_time = self.StrToUnixTime(status.created_at)
                         if current_time <= self.dict_time[account]:
@@ -72,6 +74,7 @@ class Data(object) :
         finally:
             return list_ref
 
+    # 텔레그램에 메시지 보내기
     def SendTelegram(self, list_msg):
         if Data.telegram_bot is None:
             print("Disconnect Telegram")
@@ -80,6 +83,7 @@ class Data(object) :
                 for msg in list_msg:
                     Data.telegram_bot.sendMessage(chat_id=account, text=msg)
 
+    # 특정 계정 타임라인 새로운 트윗 올라옴 -> 텔레그램 채널방에 뿌리기
     def StartOpperation(self):
         self.working = True;
         while self.working:
@@ -87,10 +91,10 @@ class Data(object) :
                 list_msg = self.GetTwitterData(account)
                 if len(list_msg) > 0:
                     self.SendTelegram(list_msg)
-                    print("send a message through telegram, total number of message = ", len(list_msg))
+                    print("["+account+"] send a message through telegram, total number of message = ", len(list_msg))
                 else:
-                    print("empty list_ref")
-            time.sleep(5)
+                    print("["+account+"] empty list_ref")
+            time.sleep(Data.sleep_second)
 
     def StopOpperation(self):
         self.working = False;
@@ -107,6 +111,7 @@ class Data(object) :
                 Data.list_account = json_data["parse_account"]
                 Data.telegram_token = json_data["telegram_token"]
                 Data.list_telegram_account = json_data["telegram_account"]
+                Data.sleep_second = json_data["sleep_second"]
 
         except Exception as e:
             print(e)
